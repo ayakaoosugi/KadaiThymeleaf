53cbf4e522fe67c588bff861ffd954a11156ed90
ayakaoosugi <tantaran924@gmail.com>
2023-02-17 17:59:03 +0900

    課題コミット

---
+++ src/main/java/com/techacademy/OutputController.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OutputController {
+
+    @GetMapping("/output")
+    public String getLink(Model model) {
+        model.addAttribute("id", "あいうえお");
+        return "output";}}
